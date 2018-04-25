3a11d313e05aa4e144d851415c31fbb6f3b3b2b7
timperi.emil <timperi.emil@SEMS-259-08.SEMS259.LAB>
2018-04-25 14:57:34 -0700

    String1, PiAloud

--- src/TestScores.java
+++ src/TestScores.java
@@ -4,7 +4,7 @@ public class TestScores {
 public static void main(String[] args) {
 	String Score = JOptionPane.showInputDialog("What did u get on your latest test?");
 	double testScore = Double.parseDouble(Score);
-	if (testScore >= 90.0) {
+	if (testScore >= 80.0) {
 		JOptionPane.showMessageDialog(null, "Good Job!");
 	} else {
 		JOptionPane.showMessageDialog(null, "Maybe you should study more.");
